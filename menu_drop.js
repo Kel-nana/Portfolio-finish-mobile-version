2dd5949a12f227332ab55d6f5e2c3af9cd9a7b3b
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-02-04 09:42:35 +0300

    Addition of background close functionality in menu_drop.js

--- menu_drop.js
+++ menu_drop.js
@@ -7,25 +7,34 @@ const bottomLine = document.querySelector('.bottom_line');
 const menuLink = document.querySelectorAll('.menu_link');
 
 // ==========OPEN MENU===============
-menu.addEventListener('click', ()=> {
-    slideIn.style.width ='100vw';
-    bottomLine.style.display ='block';
-    close.style.display ='block';
-    menuLink.forEach(element => {
-        element.style.display ='block';
-    });
+menu.addEventListener('click', () => {
+  slideIn.style.width = '100vw';
+  bottomLine.style.display = 'block';
+  close.style.display = 'block';
+  menuLink.forEach(element => {
+    element.style.display = 'block';
+  });
 });
 
 // =============CLOSE MENU=============
-close.addEventListener('click', ()=> {
-    slideIn.style.width ='0';
-    bottomLine.style.display ='none';
-    close.style.display ='none';
-    menuLink.forEach(element => {
-        element.style.display ='none';
-    });
+close.addEventListener('click', () => {
+  slideIn.style.width = '0';
+  bottomLine.style.display = 'none';
+  close.style.display = 'none';
+  menuLink.forEach(element => {
+    element.style.display = 'none';
+  });
 });
 
+// ========BACKGROUND CLOSE==============
+slideIn.addEventListener('click', () => {
+  slideIn.style.width = '0';
+  bottomLine.style.display = 'none';
+  close.style.display = 'none';
+  menuLink.forEach(element => {
+    element.style.display = 'none';
+  });
+});
 
 
 
