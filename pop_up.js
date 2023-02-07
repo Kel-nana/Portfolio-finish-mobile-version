96ee0d685dab1fc42db98db79739fc5e8d514c64
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-02-07 08:33:35 +0300

    Addition of arrays/ojects for storing data in pop_up.js

---
+++ pop_up.js
@@ -0,0 +1,53 @@
+
+const projectCards = [
+    {
+        card: 1,
+        name: 'Tonic',
+        name2: 'Tonic',
+        image: 'images/tonic1.svg',
+        image2: 'images/nature.svg',
+        description: `A daily selection of privately personalized reads; no accounts or
+                            sign-ups required.`,
+        technologies: ['html', 'css', 'javascript'],
+        specifics: ['CANOPY', 'Back End Dev', '2015'],
+        specifics2: ['CANOPY', 'Back End Dev', '2015'],
+    },
+    {
+        card: 2,
+        name: 'Multi-Post Stories',
+        name2: 'Multi-Post Stories',
+        image: 'images/availability.svg',
+        image2: 'images/multipost.svg',
+        description: `Experimental content creation feature that allows users to add to
+                            an existing story over the course of a day without spamming their
+                            friends.`,
+        technologies: ['html', 'css', 'javascript'],
+        specifics: ['CANOPY', 'Back End Dev', '2015'],
+        specifics2: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    },
+    {
+        card: 3,
+        name: 'Tonic',
+        name2: 'Facebook 360',
+        image: 'images/tonic2.svg',
+        image2: 'images/facebook.svg',
+        description: ` Exploring the future of media in Facebook's first Virtual Reality
+                            app; a place to discover and enjoy 360 photos and videos on Gear
+                            VR.`,
+        technologies: ['html', 'css', 'javascript'],
+        specifics: ['CANOPY', 'Back End Dev', '2015'],
+        specifics2: ['FACEBOOK', 'Full Sta ck Dev', '2015'],
+    },
+    {
+        card: 4,
+        name: 'Multi-Post Stories',
+        name2: 'Uber Navigation',
+        image: 'images/profprint.svg',
+        image2: 'images/uber.svg',
+        description: `  A smart assistant to make driving more safe, efficient, and fun by
+                            unlocking your most expensive computer: your car.`,
+        technologies: ['html', 'css', 'javascript'],
+        specifics: ['CANOPY', 'Back End Dev', '2015'],
+        specifics2: ['UBER', 'Lead Developer', '2018'],
+    },
+];
