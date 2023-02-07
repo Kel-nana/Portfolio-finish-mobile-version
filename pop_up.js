e97c81876f3669e8f68f8349cacc2e16967954a2
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-02-07 16:33:35 +0300

    Addition of console.log

--- pop_up.js
+++ pop_up.js
@@ -1,55 +1,57 @@
 const projectCards = [
-  {
-    card: 1,
-    name: 'Tonic',
-    name2: 'Tonic',
-    image: 'images/tanne.svg',
-    image2: 'images/SnapshootPortfolio.svg',
-    description: `A daily selection of privately personalized reads; no accounts or
+    {
+        card: 1,
+        name: 'Tonic',
+        name2: 'Tonic',
+        image: 'images/tanne.svg',
+        image2: 'images/SnapshootPortfolio.svg',
+        description: `A daily selection of privately personalized reads; no accounts or
                             sign-ups required.`,
-    technologies: ['html', 'css', 'javascript'],
-    specifics: ['CANOPY', 'Back End Dev', '2015'],
-    specifics2: ['CANOPY', 'Back End Dev', '2015'],
-  },
-  {
-    card: 2,
-    name: 'Multi-Post Stories',
-    name2: 'Multi-Post Stories',
-    image: 'images/availability.svg',
-    image2: 'images/tonic2.svg',
-    description: `Experimental content creation feature that allows users to add to
+        technologies: ['html', 'css', 'javascript'],
+        specifics: ['CANOPY', 'Back End Dev', '2015'],
+        specifics2: ['CANOPY', 'Back End Dev', '2015'],
+    },
+    {
+        card: 2,
+        name: 'Multi-Post Stories',
+        name2: 'Multi-Post Stories',
+        image: 'images/availability.svg',
+        image2: 'images/tonic2.svg',
+        description: `Experimental content creation feature that allows users to add to
                             an existing story over the course of a day without spamming their
                             friends.`,
-    technologies: ['html', 'css', 'javascript'],
-    specifics: ['CANOPY', 'Back End Dev', '2015'],
-    specifics2: ['FACEBOOK', 'Full Stack Dev', '2015'],
-  },
-  {
-    card: 3,
-    name: 'Tonic',
-    name2: 'Facebook 360',
-    image: 'images/gymfit.svg',
-    image2: 'images/tonic3.svg',
-    description: ` Exploring the future of media in Facebook's first Virtual Reality
+        technologies: ['html', 'css', 'javascript'],
+        specifics: ['CANOPY', 'Back End Dev', '2015'],
+        specifics2: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    },
+    {
+        card: 3,
+        name: 'Tonic',
+        name2: 'Facebook 360',
+        image: 'images/gymfit.svg',
+        image2: 'images/tonic3.svg',
+        description: ` Exploring the future of media in Facebook's first Virtual Reality
                             app; a place to discover and enjoy 360 photos and videos on Gear
                             VR.`,
-    technologies: ['html', 'css', 'javascript'],
-    specifics: ['CANOPY', 'Back End Dev', '2015'],
-    specifics2: ['FACEBOOK', 'Full Sta ck Dev', '2015'],
-  },
-  {
-    card: 4,
-    name: 'Multi-Post Stories',
-    name2: 'Uber Navigation',
-    image: 'images/tonicSmall.svg',
-    image2: 'images/tonic4.svg',
-    description: `  A smart assistant to make driving more safe, efficient, and fun by
+        technologies: ['html', 'css', 'javascript'],
+        specifics: ['CANOPY', 'Back End Dev', '2015'],
+        specifics2: ['FACEBOOK', 'Full Sta ck Dev', '2015'],
+    },
+    {
+        card: 4,
+        name: 'Multi-Post Stories',
+        name2: 'Uber Navigation',
+        image: 'images/tonicSmall.svg',
+        image2: 'images/tonic4.svg',
+        description: `  A smart assistant to make driving more safe, efficient, and fun by
                             unlocking your most expensive computer: your car.`,
-    technologies: ['html', 'css', 'javascript'],
-    specifics: ['CANOPY', 'Back End Dev', '2015'],
-    specifics2: ['UBER', 'Lead Developer', '2018'],
-  },
+        technologies: ['html', 'css', 'javascript'],
+        specifics: ['CANOPY', 'Back End Dev', '2015'],
+        specifics2: ['UBER', 'Lead Developer', '2018'],
+    },
 ];
+
+
 
 const portfolioSection = document.getElementById('main-container-desktop');
 portfolioSection.innerHTML = projectCards.map((project, index) => `
@@ -101,3 +103,4 @@ portfolioSection.innerHTML = projectCards.map((project, index) => `
         </div>
  </article>
   `).join('');
+console.log(projectCards[0].image);
