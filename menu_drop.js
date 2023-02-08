fcbf8905d0f483d4c980204213d6d2a3dea30744
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-02-08 12:45:31 +0300

    Addition Changes in functionality to use right instead of width

--- menu_drop.js
+++ menu_drop.js
@@ -1,38 +1,19 @@
-/* eslint-disable linebreak-style */
-// =======SELECTORS========
-const menu = document.querySelector('.bars');
-const slideIn = document.querySelector('.header-desktop');
-const close = document.querySelector('.fa-xmark');
-const bottomLine = document.querySelector('.bottom_line');
-const menuLink = document.querySelectorAll('.menu_link');
+const navLinks = document.getElementById('navLinks');
+const showMenu = document.getElementById('menuBar');
+const hideMenu = document.getElementById('closeIcon');
+const closeMenu = document.getElementById('navItems');
 
-// ==========OPEN MENU===============
-menu.addEventListener('click', () => {
-  slideIn.style.width = '100vw';
-  bottomLine.style.display = 'block';
-  close.style.display = 'block';
-  menuLink.forEach((elementz) => {
-    elementz.style.display = 'block';
-  });
-});
+showMenu.onclick = function showMenu() {
+  navLinks.style.right = '0';
+  navLinks.style.display = 'block';
+};
 
-// =============CLOSE MENU=============
-close.addEventListener('click', () => {
-  slideIn.style.width = '0';
-  bottomLine.style.display = 'none';
-  close.style.display = 'none';
-  menuLink.forEach((elementz) => {
-    elementz.style.display = 'none';
-  });
-});
+hideMenu.onclick = function hideMenu() {
+  navLinks.style.right = '-100vw';
+  navLinks.style.display = 'none';
+};
 
-// ========BACKGROUND CLOSE==============
-slideIn.addEventListener('click', () => {
-  slideIn.style.width = '0';
-  bottomLine.style.display = 'none';
-  close.style.display = 'none';
-  // eslint-disable-next-line arrow-parens
-  menuLink.forEach((elementz) => {
-    elementz.style.display = 'none';
-  });
-});
+closeMenu.onclick = function hideMenu() {
+  navLinks.style.right = '-100vw';
+  navLinks.style.display = 'none';
+};
