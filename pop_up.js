2055373d499b08e4be29bc1b1498e0569c39f257
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-02-08 12:50:42 +0300

    fixing linter errors

--- pop_up.js
+++ pop_up.js
@@ -1,54 +1,54 @@
 const projectCards = [
-    {
-        card: 1,
-        name: 'Tonic',
-        name2: 'Tonic',
-        image: 'images/tanne.svg',
-        image2: 'images/SnapshootPortfolio.svg',
-        description: `A daily selection of privately personalized reads; no accounts or
+  {
+    card: 1,
+    name: 'Tonic',
+    name2: 'Tonic',
+    image: 'images/tanne.svg',
+    image2: 'images/SnapshootPortfolio.svg',
+    description: `A daily selection of privately personalized reads; no accounts or
                             sign-ups required.`,
-        technologies: ['html', 'css', 'javascript'],
-        specifics: ['CANOPY', 'Back End Dev', '2015'],
-        specifics2: ['CANOPY', 'Back End Dev', '2015'],
-    },
-    {
-        card: 2,
-        name: 'Multi-Post Stories',
-        name2: 'Multi-Post Stories',
-        image: 'images/availability.svg',
-        image2: 'images/tonic2.svg',
-        description: `Experimental content creation feature that allows users to add to
+    technologies: ['html', 'css', 'javascript'],
+    specifics: ['CANOPY', 'Back End Dev', '2015'],
+    specifics2: ['CANOPY', 'Back End Dev', '2015'],
+  },
+  {
+    card: 2,
+    name: 'Multi-Post Stories',
+    name2: 'Multi-Post Stories',
+    image: 'images/availability.svg',
+    image2: 'images/tonic2.svg',
+    description: `Experimental content creation feature that allows users to add to
                             an existing story over the course of a day without spamming their
                             friends.`,
-        technologies: ['html', 'css', 'javascript'],
-        specifics: ['CANOPY', 'Back End Dev', '2015'],
-        specifics2: ['FACEBOOK', 'Full Stack Dev', '2015'],
-    },
-    {
-        card: 3,
-        name: 'Tonic',
-        name2: 'Facebook 360',
-        image: 'images/gymfit.svg',
-        image2: 'images/tonic3.svg',
-        description: ` Exploring the future of media in Facebook's first Virtual Reality
+    technologies: ['html', 'css', 'javascript'],
+    specifics: ['CANOPY', 'Back End Dev', '2015'],
+    specifics2: ['FACEBOOK', 'Full Stack Dev', '2015'],
+  },
+  {
+    card: 3,
+    name: 'Tonic',
+    name2: 'Facebook 360',
+    image: 'images/gymfit.svg',
+    image2: 'images/tonic3.svg',
+    description: ` Exploring the future of media in Facebook's first Virtual Reality
                             app; a place to discover and enjoy 360 photos and videos on Gear
                             VR.`,
-        technologies: ['html', 'css', 'javascript'],
-        specifics: ['CANOPY', 'Back End Dev', '2015'],
-        specifics2: ['FACEBOOK', 'Full Sta ck Dev', '2015'],
-    },
-    {
-        card: 4,
-        name: 'Multi-Post Stories',
-        name2: 'Uber Navigation',
-        image: 'images/tonicSmall.svg',
-        image2: 'images/tonic4.svg',
-        description: `  A smart assistant to make driving more safe, efficient, and fun by
+    technologies: ['html', 'css', 'javascript'],
+    specifics: ['CANOPY', 'Back End Dev', '2015'],
+    specifics2: ['FACEBOOK', 'Full Sta ck Dev', '2015'],
+  },
+  {
+    card: 4,
+    name: 'Multi-Post Stories',
+    name2: 'Uber Navigation',
+    image: 'images/tonicSmall.svg',
+    image2: 'images/tonic4.svg',
+    description: `  A smart assistant to make driving more safe, efficient, and fun by
                             unlocking your most expensive computer: your car.`,
-        technologies: ['html', 'css', 'javascript'],
-        specifics: ['CANOPY', 'Back End Dev', '2015'],
-        specifics2: ['UBER', 'Lead Developer', '2018'],
-    },
+    technologies: ['html', 'css', 'javascript'],
+    specifics: ['CANOPY', 'Back End Dev', '2015'],
+    specifics2: ['UBER', 'Lead Developer', '2018'],
+  },
 ];
 
 const portfolioArticle = document.getElementById('portfolio');
@@ -112,10 +112,10 @@ const descriptionModal = document.querySelector('.modal-desc p');
 const categoriesModal = document.querySelector('.modal-categories');
 
 const portfolio = [
-    {
-        name: 'Tonic',
-        image: 'images/SnapshootPortfolio.svg',
-        description: ` Lorem Ipsum is simply dummy text of the printing and
+  {
+    name: 'Tonic',
+    image: 'images/SnapshootPortfolio.svg',
+    description: ` Lorem Ipsum is simply dummy text of the printing and
               typesetting industry. Lorem Ipsum has been the industry's
               standard dummy text ever since the 1500s, when an unknown
               printer took a galley of type and scrambled it 1960s with
@@ -132,12 +132,12 @@ const portfolio = [
               industry's standard dummy text ever since the 1500s, when an
               unknown printer took a galley of type and scrambled it 1960s
               with the release.`,
-        technologies: ['html', 'css', 'javascript', 'bootstrap', 'ruby', 'github'],
-    },
-    {
-        name: 'Multi-Post Stories',
-        image: 'images/tonic3.svg',
-        description: ` Lorem Ipsum is simply dummy text of the printing and
+    technologies: ['html', 'css', 'javascript', 'bootstrap', 'ruby', 'github'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    image: 'images/tonic3.svg',
+    description: ` Lorem Ipsum is simply dummy text of the printing and
               typesetting industry. Lorem Ipsum has been the industry's
               standard dummy text ever since the 1500s, when an unknown
               printer took a galley of type and scrambled it 1960s with
@@ -154,12 +154,12 @@ const portfolio = [
               industry's standard dummy text ever since the 1500s, when an
               unknown printer took a galley of type and scrambled it 1960s
               with the release.`,
-        technologies: ['html', 'css', 'javascript', 'bootstrap', 'ruby', 'github'],
-    },
-    {
-        name: 'Facebook 360',
-        image: 'images/tonic4.svg',
-        description: ` Lorem Ipsum is simply dummy text of the printing and
+    technologies: ['html', 'css', 'javascript', 'bootstrap', 'ruby', 'github'],
+  },
+  {
+    name: 'Facebook 360',
+    image: 'images/tonic4.svg',
+    description: ` Lorem Ipsum is simply dummy text of the printing and
               typesetting industry. Lorem Ipsum has been the industry's
               standard dummy text ever since the 1500s, when an unknown
               printer took a galley of type and scrambled it 1960s with
@@ -176,12 +176,12 @@ const portfolio = [
               industry's standard dummy text ever since the 1500s, when an
               unknown printer took a galley of type and scrambled it 1960s
               with the release.`,
-        technologies: ['html', 'css', 'javascript', 'bootstrap', 'ruby', 'github'],
-    },
-    {
-        name: 'Uber Navigation',
-        image: 'images/tonic4.svg',
-        description: ` Lorem Ipsum is simply dummy text of the printing and
+    technologies: ['html', 'css', 'javascript', 'bootstrap', 'ruby', 'github'],
+  },
+  {
+    name: 'Uber Navigation',
+    image: 'images/tonic4.svg',
+    description: ` Lorem Ipsum is simply dummy text of the printing and
               typesetting industry. Lorem Ipsum has been the industry's
               standard dummy text ever since the 1500s, when an unknown
               printer took a galley of type and scrambled it 1960s with
@@ -198,54 +198,53 @@ const portfolio = [
               industry's standard dummy text ever since the 1500s, when an
               unknown printer took a galley of type and scrambled it 1960s
               with the release.`,
-        technologies: ['html', 'css', 'javascript', 'bootstrap', 'ruby', 'github'],
-    },
+    technologies: ['html', 'css', 'javascript', 'bootstrap', 'ruby', 'github'],
+  },
 ];
 
 // ===================OPEN MODUL===========
 function windowModelUpdate(index) {
-    categoriesModal.innerHTML = '';
-    titleModal.innerHTML = portfolio[index].name;
-    imageModal.src = portfolio[index].image;
-    descriptionModal.innerHTML = portfolio[index].description;
-    portfolio[index].technologies.forEach((technology) => {
-        const li = document.createElement('li');
-        li.innerText = technology;
-        categoriesModal.appendChild(li);
-    });
+  categoriesModal.innerHTML = '';
+  titleModal.innerHTML = portfolio[index].name;
+  imageModal.src = portfolio[index].image;
+  descriptionModal.innerHTML = portfolio[index].description;
+  portfolio[index].technologies.forEach((technology) => {
+    const li = document.createElement('li');
+    li.innerText = technology;
+    categoriesModal.appendChild(li);
+  });
 }
 
-
 function openModal(modal) {
-    if (modal == null) return;
-    modal.classList.add('active');
-    overlay.classList.add('active');
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
 }
 
 function closeModal(modal) {
-    if (modal == null) return;
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
 }
 
 openModelbtn.forEach((button) => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modal);
-        openModal(modal);
-        windowModelUpdate(Array.prototype.indexOf.call(openModelbtn, button));
-    });
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modal);
+    openModal(modal);
+    windowModelUpdate(Array.prototype.indexOf.call(openModelbtn, button));
+  });
 });
 
 overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active');
-    modals.forEach((modal) => {
-        closeModal(modal);
-    });
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
 });
 
 closeModelbtn.forEach((button) => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal');
-        closeModal(modal);
-    });
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
 });
