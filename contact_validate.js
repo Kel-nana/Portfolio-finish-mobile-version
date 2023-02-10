64d2c4b6de4ea4c574f3648582c63ddf71949d36
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-02-10 11:31:32 +0300

    This project was done in collaoration with Baqar Abbas

--- contact_validate.js
+++ contact_validate.js
@@ -2,9 +2,12 @@ const form = document.querySelector('form');
 const email = document.querySelector('#email');
 const popUp = document.querySelector('.popup');
 const test = document.querySelector('.test');
+
 form.addEventListener('submit', (e) => {
   const emailRegex = /[A-Z]+/;
+
   const emailValue = email.value;
+
   if (emailRegex.test(emailValue)) {
     popUp.querySelector('p').innerHTML = 'Please ensure your email is in lowercase';
     test.style.display = 'block';
